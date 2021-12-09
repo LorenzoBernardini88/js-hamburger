cba468c0e709266efbf2f50c08e13ea6ea254613
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2021-12-09 16:56:28 +0100

    commento tentativo seconda soluione

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-// const btnOpen = document.querySelector(".fas.fa-bars");
-// const btnClose = document.querySelector(".fas.fa-times");
+const btnOpen = document.querySelector(".fas.fa-bars");
+const btnClose = document.querySelector(".fas.fa-times");
+let hambMenu = document.querySelector(".hamburger-menu");
+
+
+btnOpen.addEventListener("click",function(){
+    hambMenu.classList.add("active");
+});
+
+btnClose.addEventListener("click",function(){
+    hambMenu.classList.remove("active");
+});
+
+// const btnMenu = document.getElementsByClassName(".fas");
 // let hambMenu = document.querySelector(".hamburger-menu");
 
+// btnMenu.addEventListener("click", function(){
+//     if(hambMenu.style.display == "none"){
+//         hambMenu.classList.add("active");
+    
+//     } else {
+//         hambMenu.classList.remove("active");
+//     }
 
-// btnOpen.addEventListener("click",function(){
-//     hambMenu.classList.add("active");
-// });
-
-// btnClose.addEventListener("click",function(){
-//     hambMenu.classList.remove("active");
-// });
+// })
