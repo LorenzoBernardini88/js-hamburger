d3df6f26ccf1e4903c1606e446950270db128be4
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2021-12-09 16:22:04 +0100

    aggiungo click function a btnOpen e btnClose

---
+++ main.js
@@ -0,0 +1,12 @@
+const btnOpen = document.querySelector(".fas.fa-bars");
+const btnClose = document.querySelector(".fas.fa-times");
+let hambMenu = document.querySelector(".hamburger-menu");
+
+
+btnOpen.addEventListener("click",function(){
+    hambMenu.classList.add("active");
+});
+
+btnClose.addEventListener("click",function(){
+    hambMenu.classList.remove("active");
+});
